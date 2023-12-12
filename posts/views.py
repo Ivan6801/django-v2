717cef0f7aa7e26268ac9a4248180bce2b2e04c9
Ivan Gonzalez <hal308366@gmail.com>
2023-12-12 12:52:38 -0800

    Cambiar el texto

--- posts/views.py
+++ posts/views.py
@@ -11,14 +11,14 @@ posts = [
     {
         'title': 'Mont Blanc',
         'user': {
-            'name': 'YÃ©sica CortÃ©s',
+            'name': 'Yasica Cortas',
             'picture': 'https://picsum.photos/60/60/?image=1027'
         },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
         'photo': 'https://picsum.photos/800/600?image=1036',
     },
     {
-        'title': 'Via LÃ¡ctea',
+        'title': 'Via lactea',
         'user': {
             'name': 'Christian Van der Henst',
             'picture': 'https://picsum.photos/60/60/?image=1005'
